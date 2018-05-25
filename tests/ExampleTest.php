bfa79391a4e85f127de8b0e3da06dc0b83471fa4
Hala Grant <halagrant@hotmail.com>
2018-05-25 17:27:07 +0200

    Adding some test cases

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,12 +5,73 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    
+
+     public function testSomethingIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+//    
+//    public function testDatabase()
+//{
+//    // Make call to application...
+//
+//    $this->seeInDatabase('product', ['name' => 'Item1']);
+//}
+  public function testBasicExample()
+    {
+   
+
+        $this->json('POST', '/api/v1/product', 
+                ['name' => 'item1',
+            'category' => 'cat1',
+            'subcategor'=> 200,
+            'collection_id' => 7         
+            ])->seeInDatabase('product', ['name' => 'item1']);
+    }
+    
+    public function testB()
+    {
+   
+$this->json('GET', '/api/v1/product/')->seeInDatabase('product', ['name' => 'item1']);
+//             ->seeJson([
+//                'name' => 'item1',
+//             ]);
+    }
+//    public function testThatNameFieldIsRequiredToCreateAUser()
+//    {
+//        $this->json(
+//            'POST', '/product', [
+//                'name' => "haha"
+//            ]
+//        )->seeJson(
+//            [
+//                'name' => [
+//                    'The name field is required.'
+//                ]
+//            ]
+//        )->seeStatusCode(422);
+//    }
+
+  public function testCanGetAllProducts()
+{
+//       $this->get('/api/v1/product/');
+
+//        $this->assertEquals(
+//            $this->app->version(), $this->response->getContent()
+//        );
+  $this->json('GET', '/api/v1/product/')->seeStatusCode(200);
+}
+
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testExample()
+    
+  
+public function testExample()
     {
         $this->get('/');
 
@@ -18,4 +79,6 @@ class ExampleTest extends TestCase
             $this->app->version(), $this->response->getContent()
         );
     }
+    
+ 
 }
