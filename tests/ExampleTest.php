c17bef7249ea7df61eca79b195ee14b87b24b1b9
Hala Grant <halagrant@hotmail.com>
2018-05-25 19:28:22 +0200

    Adding some test cases  2

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,82 +3,95 @@
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
-{
-    
+class ExampleTest extends TestCase {
 
-     public function testSomethingIsTrue()
-    {
+    public function testSomethingIsTrue() {
         $this->assertTrue(true);
     }
-//    
-//    public function testDatabase()
-//{
-//    // Make call to application...
-//
-//    $this->seeInDatabase('product', ['name' => 'Item1']);
-//}
-  public function testBasicExample()
-    {
-   
 
-        $this->json('POST', '/api/v1/product', 
-                ['name' => 'item1',
+    public function testInsertAProduct() {
+        $this->json('POST', '/api/v1/product', ['name' => 'item1',
             'category' => 'cat1',
-            'subcategor'=> 200,
-            'collection_id' => 7         
-            ])->seeInDatabase('product', ['name' => 'item1']);
+            'subcategor' => 'cat2',
+            'value' => 200,
+            'collection_id' => 7,
+            'tags' => [
+                'porsche',
+                'design'
+            ]
+        ])->seeInDatabase('product', ['name' => 'item1']);
     }
-    
-    public function testB()
-    {
-   
-$this->json('GET', '/api/v1/product/')->seeInDatabase('product', ['name' => 'item1']);
-//             ->seeJson([
-//                'name' => 'item1',
-//             ]);
+
+    /*
+     * check returned json contains item1 insearted earlier
+     */
+
+    public function testcheckingAProduct() {
+        $this->json('GET', '/api/v1/product/')->seeJSON(['name' => 'item1']);
     }
-//    public function testThatNameFieldIsRequiredToCreateAUser()
-//    {
-//        $this->json(
-//            'POST', '/product', [
-//                'name' => "haha"
-//            ]
-//        )->seeJson(
-//            [
-//                'name' => [
-//                    'The name field is required.'
-//                ]
-//            ]
-//        )->seeStatusCode(422);
-//    }
 
-  public function testCanGetAllProducts()
-{
-//       $this->get('/api/v1/product/');
+    public function testCanManageProducts() {
+        $this->json('POST', '/api/v1/order', [
+            'parameters' => [
 
-//        $this->assertEquals(
-//            $this->app->version(), $this->response->getContent()
-//        );
-  $this->json('GET', '/api/v1/product/')->seeStatusCode(200);
-}
+                "order" => [
 
+                    "items" => [ '*' => [
+                            "name" => "Item1",
+                            "qnt" => 1,
+                            "value" => 1100,
+                            "category" => "Fashion",
+                            "subcategory" => "Jacket",
+                            "tags" =>
+                            ["porsche", "design"],
+                            "collection_id" => 7
+                        ]
+                    ]
+                ]
+            ]
+                ]
+        )->seeStatusCode(200); 
+    }
+    public function testCanCalculateTheTotalPaymentAfterDiscountProducts() {
+        $this->json('POST', '/api/v1/order', [
+            'parameters' => [
+
+                "order" => [
+
+                    "items" => [ '*' => [
+                            "name" => "Item1",
+                            "qnt" => 1,
+                            "value" => 1100,
+                            "category" => "Fashion",
+                            "subcategory" => "Jacket",
+                            "tags" =>
+                            ["porsche", "design"],
+                            "collection_id" => 7
+                        ]
+                    ]
+                ]
+            ]
+                ]
+        )->seeJSON(['total_payment_after_discount' => 1001]);
+    }
+
+ 
+
+    public function testCanGetAllProducts() {
+        $this->json('GET', '/api/v1/product/')->seeStatusCode(200);
+    }
 
     /**
      * A basic test example.
      *
      * @return void
      */
-    
-  
-public function testExample()
-    {
+    public function testExample() {
         $this->get('/');
 
         $this->assertEquals(
-            $this->app->version(), $this->response->getContent()
+                $this->app->version(), $this->response->getContent()
         );
     }
-    
- 
+
 }
